8e1c265538410e40890ce446b14f2753ea5589e4
EB <espressobeanies@gmail.com>
2019-02-11 22:47:29 -0500

    Create intall.sh
    Updated README and install script

---
+++ intall.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#Email
+yum install mutt
+
+#Text web-browsers
+yum install dillo lynx
+
+#Instant Messaging
+yum install finch
+
+#Music player
+yum install cmus
+
+#ASCII image browser
+yum install caca-utils
+
+#Bittorrent
+yum install rtorrent
+
+#Git projects
+yum install git
+
+#CSV spreadsheet viewer
+git clone https://github.com/evert/csv123.git
+
+#News readers
+git clone https://github.com/dansomething/vim-hackernews.git
+
+#Miscellaneous tui-based tools
+yum install htop nethogs NetworkManager-tui system-config-firewall-tui
